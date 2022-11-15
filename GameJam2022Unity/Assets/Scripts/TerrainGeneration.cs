141d0b563c4847fa8a6dfa994cbb5ba4032c135e
Robert Cockell <reddragonmakesgames@gmail.com>
2022-11-15 16:25:55 +0000

    Added terrain generation. Also made Red's hitbox slightly smaller to allow her to jump through a 1 block gap and made her jump slightly higher so she can easily make it onto a platform she can walk under.

---
+++ GameJam2022Unity/Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    public Tile groundTile;
+    public Tile groundSlopeUpTile;
+    public Tile groundSlopeDownTile;
+    public Tile invisibleWall;
+    public Tile platformTile;
+    public int stageSize = 200;
+    public int leftSize = 20;
+    public int invisibleWallHeight = 20;
+    public int gridDepth = 10;
+    public int generationStartPoint = 5;
+    public int groundMinHeight = -3;
+    public int groundMaxHeight = 3;
+    public int platformMaxHeight = 10;
+    public int heightChangeChance = 4;
+    public int platformsToSpawn = 15;
+    public int platformsSize = 3;
+    public int platformSpacing = 3;         //The minimum space under each platform
+
+    private Tilemap map;
+    private int maxTriesBeforeFailure = 50;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GetComponent<Tilemap>();
+
+        //Draw invisible walls at edges
+        for (int i = -gridDepth; i < invisibleWallHeight; i++)
+        {
+            map.SetTile(new Vector3Int(-leftSize, i), invisibleWall);
+            map.SetTile(new Vector3Int(stageSize, i), invisibleWall);
+        }
+
+        //Draw ground from left to the spawn position
+        for (int i = -leftSize; i < generationStartPoint; i++)
+        {
+            for (int j = 0; j > -gridDepth; j--)
+            {
+                map.SetTile(new Vector3Int(i, j), groundTile);
+            }
+        }
+        int currentHeight = 0;
+        //Flags to stop quick changes in direction
+        bool wentDown = false;
+        bool wentUp = false;
+        //Draw ground, moving up and down randomly
+        for (int i = generationStartPoint; i < stageSize; i++)
+        {
+            int heightChange = Random.Range(0, 1 + heightChangeChance);
+            if (heightChange == 0 && currentHeight > groundMinHeight && !wentUp)
+            {
+                //Add slope tile
+                map.SetTile(new Vector3Int(i, currentHeight), groundSlopeDownTile);
+                currentHeight--;
+                wentDown = true;
+                wentUp = false;
+            }
+            else if (heightChange == heightChangeChance && currentHeight < groundMaxHeight && !wentDown)
+            { 
+                currentHeight++;
+                wentUp = true;
+                wentDown = false;
+                //Add slope tile
+                map.SetTile(new Vector3Int(i - 1, currentHeight), groundSlopeUpTile);
+            }
+            else
+            {
+                wentDown = false;
+                wentUp = false;
+            }
+            //Fill in ground, all the way to the bottom
+            for (int j = currentHeight; j > -gridDepth; j--)
+            {
+                map.SetTile(new Vector3Int(i, j), groundTile);
+            }
+        }
+
+        //Add upper platforms for platforming
+        for (int i = 0; i < platformsToSpawn; i++) 
+        {
+            bool valid = false;
+            int triesBeforeFailure = maxTriesBeforeFailure;
+            while (valid == false)
+            {
+                //Platform size +2 because you have to consider the sides to tell if it's a valid platform
+                int xcord = Random.Range(generationStartPoint, stageSize - (platformsSize + 2));
+                int spawnLevel = 0;
+                while (spawnLevel < platformMaxHeight)
+                {
+                    valid = true;
+                    for (int j = 0; j < platformsSize + 2; j++)
+                    {
+                        for (int k = spawnLevel - (platformSpacing); k < spawnLevel + (platformSpacing + 1); k++)
+                        {
+                            if (map.HasTile(new Vector3Int(xcord + j, k)))
+                            {
+                                valid= false;
+                                break;
+                            }
+                        }
+                        if (valid == false)
+                        {
+                            break;
+                        }
+                    }
+                    if (valid == true)
+                    {
+                        //There is an empty space under and above this area, so we can spawn a platform
+                        SpawnPlatform(xcord + 1, spawnLevel);
+                        break;
+                    }
+                    else
+                    {
+                        spawnLevel++;
+                        triesBeforeFailure--;
+                        if (triesBeforeFailure < 0)
+                        {
+                            Debug.LogError("Spawning platforms failed, no valid platform spot found after " + maxTriesBeforeFailure + " tries");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void SpawnPlatform(int x, int y)
+    {
+        for (int i = 0; i < platformsSize; i++)
+        {
+            map.SetTile(new Vector3Int(x + i, y), platformTile);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
