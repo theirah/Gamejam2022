766642531cd38713d8ca8b850f0fc1f1d382af16
Robert Cockell <reddragonmakesgames@gmail.com>
2022-11-18 18:53:04 +0000

    Added a basic system to spawn attack prefabs that will handle the hitboxes for attacks

--- GameJam2022Unity/Assets/Scripts/Enemy AI/Boar.cs
+++ GameJam2022Unity/Assets/Scripts/Enemy AI/Boar.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Boar : MonoBehaviour
 {
     public int attackDistance = 3;
+    public GameObject chargeAttack;
+    private GameObject chargeAttackInstance = null;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +29,29 @@ public class Boar : MonoBehaviour
         {
             if (Vector3.Magnitude(hit.transform.position - transform.position) < attackDistance)
             {
-                Debug.Log("Pig attacking!");
+                //Debug.Log("Pig attacking!");
+                if (chargeAttackInstance == null) 
+                {
+                    ChargeAttack();
+                }
+                
             }
         }
         else
         {
-            Debug.Log("Pig can't see player");
+            //Debug.Log("Pig can't see player");
         }
+    }
+
+    void ChargeAttack()
+    {
+        if (chargeAttack == null)
+        {
+            Debug.LogError("No attack found");
+            return;
+        }
+
+        chargeAttackInstance = Instantiate(chargeAttack, this.transform.position, this.transform.rotation);
+        HitboxLifetime lifetime = chargeAttackInstance.GetComponent<HitboxLifetime>();
     }
 }
