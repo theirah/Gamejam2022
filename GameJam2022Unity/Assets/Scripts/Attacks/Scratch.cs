1b7ecc7d5f96982eeef7bcdc5a960fda00bc948f
Robert Cockell <reddragonmakesgames@gmail.com>
2022-11-22 10:52:20 +0000

    Set wolf's attack to be a trigger, and made it so only one instance would be present at any given time, also set its lifetime

--- GameJam2022Unity/Assets/Scripts/Attacks/Scratch.cs
+++ GameJam2022Unity/Assets/Scripts/Attacks/Scratch.cs
@@ -11,13 +11,17 @@ public class Scratch : Weapon
     GameObject hitboxRepresentation; // the object representing the hitbox and animation associated with the struck location
     [SerializeField]
     float cooldown;
+    private GameObject hitbox = null;
 
     public void Fire(Vector3 initiatorPosition, Vector3 targetPosition)
     {
-        int offsetDirection = targetPosition.x >= initiatorPosition.x ? 1 : -1;
-        Vector3 instanceOffset = spawnOffset;
-        instanceOffset.x *= offsetDirection;
-        GameObject hitbox = Instantiate(hitboxRepresentation, initiatorPosition + instanceOffset, Quaternion.identity);
-        hitbox.transform.localScale = new Vector3(offsetDirection* hitbox.transform.localScale.x, hitbox.transform.localScale.y, hitbox.transform.localScale.z);
+        if (hitbox == null) 
+        {
+            int offsetDirection = targetPosition.x >= initiatorPosition.x ? 1 : -1;
+            Vector3 instanceOffset = spawnOffset;
+            instanceOffset.x *= offsetDirection;
+            hitbox = Instantiate(hitboxRepresentation, initiatorPosition + instanceOffset, Quaternion.identity);
+            hitbox.transform.localScale = new Vector3(offsetDirection * hitbox.transform.localScale.x, hitbox.transform.localScale.y, hitbox.transform.localScale.z);
+        }
     }
 }
