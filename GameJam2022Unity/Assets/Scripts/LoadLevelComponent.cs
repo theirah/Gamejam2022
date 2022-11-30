e8b25392e24f648aa49042d32b17b2f349bff309
Theirah <theirahhkhah@gmail.com>
2022-11-30 01:36:13 -0800

    Made copies of Sample scene and renamed them level1, level2, and level3. Hooked up corpses to dialogue and loading the next level Removed facing direction requirement for interactions

---
+++ GameJam2022Unity/Assets/Scripts/LoadLevelComponent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelComponent : MonoBehaviour
+{
+    public string levelName; // Name of level to load when LoadLevel is called
+    public void LoadLevel()
+    {
+        if (levelName.Length > 0)
+        {
+            AudioManager audioManager = FindObjectOfType<AudioManager>();
+            audioManager.StageEnd();
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
