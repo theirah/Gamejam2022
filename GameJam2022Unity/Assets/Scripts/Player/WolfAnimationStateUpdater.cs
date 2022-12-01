87d9bfe3d24bbf5db8f0d5e859af0397d275602d
Theirah <theirahhkhah@gmail.com>
2022-12-01 06:35:03 -0800

    Hook up jumping animations to both characters

--- GameJam2022Unity/Assets/Scripts/Player/WolfAnimationStateUpdater.cs
+++ GameJam2022Unity/Assets/Scripts/Player/WolfAnimationStateUpdater.cs
@@ -10,22 +10,40 @@ public class WolfAnimationStateUpdater : MonoBehaviour
     [SerializeField]
     PlayerInputComponent playerInput;
 
+    [SerializeField]
+    CharacterController2D characterController;
+
     bool mIsMovingHorizontally;
+    bool mJumped;
+    bool mWasGrounded;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput.isMovingHorizontally += HandleIsMovingHorizontally;
+        playerInput.hasJumped += HandleHasJumped;
+        mJumped = false;
+        bool mWasGrounded = characterController.m_Grounded;
     }
 
     // Update is called once per frame
     void Update()
     {
         animator.SetBool("IsWalking", mIsMovingHorizontally);
+        animator.SetBool("IsGrounded", characterController.m_Grounded);
+        bool hasLanded = !mWasGrounded && characterController.m_Grounded;
+        mJumped = mJumped && !hasLanded;
+        animator.SetBool("Jumped", mJumped);
+        mWasGrounded = characterController.m_Grounded;
     }
 
     public void HandleIsMovingHorizontally(bool isMoving, float axisInput)
     {
         mIsMovingHorizontally = isMoving;
+    }
+
+    public void HandleHasJumped()
+    {
+        mJumped = true;
     }
 }
